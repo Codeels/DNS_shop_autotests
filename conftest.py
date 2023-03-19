8d1218c91a6f60b97b2bc9d4a6d055a5cd231142
Codeas <ilyas.kodirov@gmail.com>
2023-03-19 21:20:55 +0500

    Изменения Хромдрайвер берется из репозитория Добавил хромдрайвер для линукса Изменил очередность шагов в main.yml

--- conftest.py
+++ conftest.py
@@ -1,33 +1,33 @@
-# import pytest as pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-#
-#
-# @pytest.fixture(scope="function")
-# def driver():
-#     print("\nbrowser open")
-#     link = "https://www.dns-shop.ru/"
-#     driver = webdriver.Chrome(service=Service("DNS_shop\\chromedriver.exe"))
-#     driver.get(link)
-#     driver.maximize_window()
-#     yield driver
-#     driver.quit()
-#     print("\nbrowser close")
-import time
-
-import pytest
+import pytest as pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope="function")
 def driver():
     print("\nbrowser open")
     link = "https://www.dns-shop.ru/"
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.maximize_window()
+    driver = webdriver.Chrome(service=Service("DNS_shop\\chromedriver"))
     driver.get(link)
+    driver.maximize_window()
     yield driver
     driver.quit()
     print("\nbrowser close")
+
+
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+#
+# @pytest.fixture(scope="function")
+# def driver():
+#     print("\nbrowser open")
+#     link = "https://www.dns-shop.ru/"
+#     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+#     driver.maximize_window()
+#     driver.get(link)
+#     yield driver
+#     driver.quit()
+#     print("\nbrowser close")
