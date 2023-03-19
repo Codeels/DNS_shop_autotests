6fa2dc2e7e89c97a6263bd344b2511af147d5dda
Codeas <ilyas.kodirov@gmail.com>
2023-03-19 21:25:33 +0500

    Изменения Загрузил хромдрайвер

--- conftest.py
+++ conftest.py
@@ -7,7 +7,8 @@ from selenium.webdriver.chrome.service import Service
 def driver():
     print("\nbrowser open")
     link = "https://www.dns-shop.ru/"
-    driver = webdriver.Chrome(service=Service("DNS_shop\\chromedriver"))
+    # driver = webdriver.Chrome(service=Service("DNS_shop\\chromedriver"))
+    driver = webdriver.Chrome(service=Service("\\chromedriver"))
     driver.get(link)
     driver.maximize_window()
     yield driver
