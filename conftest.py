eaf9c14c70ccb741747368094f711f129072b609
Codeas <ilyas.kodirov@gmail.com>
2023-03-19 23:20:15 +0500

    Изменения conftest

--- conftest.py
+++ conftest.py
@@ -44,16 +44,17 @@ from selenium.webdriver.chrome.service import Service
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
+# options = [
+#     "--headless",
+#     "--disable-gpu",
+#     "--window-size=1920,1200",
+#     "--ignore-certificate-errors",
+#     "--disable-extensions",
+#     "--no-sandbox",
+#     "--disable-dev-shm-usage"
+# ]
+# for option in options:
+#     chrome_options.add_argument(option)
 
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
