39ec350001de487314214c7864ffd1ba3c47cbb0
Codeas <ilyas.kodirov@gmail.com>
2023-03-19 20:39:43 +0500

    Изменения conftest.py Изменен браузер на chromium

--- conftest.py
+++ conftest.py
@@ -17,15 +17,16 @@ import time
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.core.utils import ChromeType
 
 
 @pytest.fixture(scope="function")
 def driver():
     print("\nbrowser open")
     link = "https://www.dns-shop.ru/"
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.maximize_window()
     driver.get(link)
     yield driver
