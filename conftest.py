13969ccd2b7ff160c07b3b1eead8b97e11e7b209
Codeas <ilyas.kodirov@gmail.com>
2023-03-19 21:07:23 +0500

    Изменения conftest.py

--- conftest.py
+++ conftest.py
@@ -17,16 +17,15 @@ import time
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 
 
 @pytest.fixture(scope="function")
 def driver():
     print("\nbrowser open")
     link = "https://www.dns-shop.ru/"
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     driver.get(link)
     yield driver
