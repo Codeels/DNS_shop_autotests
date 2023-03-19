e9906e8cad1f05c32bd2236705f117a7ffd418b1
Codeas <ilyas.kodirov@gmail.com>
2023-03-19 21:34:05 +0500

    Изменения Вернул изменения в main.yml и conftest.py обратно

--- conftest.py
+++ conftest.py
@@ -1,34 +1,34 @@
-import pytest as pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-
-@pytest.fixture(scope="function")
-def driver():
-    print("\nbrowser open")
-    link = "https://www.dns-shop.ru/"
-    # driver = webdriver.Chrome(service=Service("DNS_shop\\chromedriver"))
-    driver = webdriver.Chrome(service=Service("\\chromedriver"))
-    driver.get(link)
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-    print("\nbrowser close")
-
-
-# import pytest
+# import pytest as pytest
 # from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
 #
 #
 # @pytest.fixture(scope="function")
 # def driver():
 #     print("\nbrowser open")
 #     link = "https://www.dns-shop.ru/"
-#     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-#     driver.maximize_window()
+#     # driver = webdriver.Chrome(service=Service("DNS_shop\\chromedriver"))
+#     driver = webdriver.Chrome(service=Service("\\chromedriver"))
 #     driver.get(link)
+#     driver.maximize_window()
 #     yield driver
 #     driver.quit()
 #     print("\nbrowser close")
+
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")
+def driver():
+    print("\nbrowser open")
+    link = "https://www.dns-shop.ru/"
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.get(link)
+    yield driver
+    driver.quit()
+    print("\nbrowser close")
